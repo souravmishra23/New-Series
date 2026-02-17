fd4113f496d2ac432cede059d3e73863114260cf
souravmishra <mishrasourav1998@gmail.com>
2026-02-17 21:40:13 +0530

    reverse array questions

--- src/arrays/RotateArray.java
+++ src/arrays/RotateArray.java
@@ -5,18 +5,27 @@ import java.util.Arrays;
 public class RotateArray {
     public static void main(String[] args) {
         int [] arr = new int[]{1,2,3,4,5,6,7};
-        rotate(arr, 3);
+        rightRotate(arr, 3);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void rotate(int[] nums, int k) {
+    public static void rightRotate(int[] nums, int k) {
         int len = nums.length;
         k %= len;
-
+        // reverse whole array
         reverse(nums, 0, len - 1);
+        // reverse first k - 1 array
+        reverse(nums, 0, k - 1);
+        // reverse first k - 1 to len
+        reverse(nums, k, len - 1);
+    }
 
+    public static void leftRotate(int [] nums, int k) {
+        int len = nums.length;
+        k %= len;
         reverse(nums, 0, k - 1);
         reverse(nums, k, len - 1);
+        reverse(nums, 0, len - 1);
     }
 
     public static void reverse(int [] nums, int start, int end) {
